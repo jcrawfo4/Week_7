71d90c0950dbb45184bf31d0e0a8181eb7377465
Jacob Crawford <jkbcrawford@gmail.com>
2024-05-30 13:29:46 -0500

    second commit with print statements

--- src/startHomework.js
+++ src/startHomework.js
@@ -5,11 +5,18 @@ const ages = [3, 9, 23, 64, 2, 8, 28, 93];
 const firstElement = ages[0];
 const lastElement = ages[ages.length - 1];
 const difference = lastElement - firstElement;
+console.log("difference: ", difference);
 // Do not use numbers to reference the last element, find it programmatically.
 // ages[7] - ages[0] is not allowed!
 // Add a new age to your array and repeat the step above to ensure it is dynamic. (works for arrays of different lengths).
 // Use a loop to iterate through the array and calculate the average age.
-
+let ageTotal = 0;
+for (let i = 0; i < ages.length; i++) {
+  ageTotal += ages[i];
+}
+const averageAge = ageTotal / ages.length;
+console.log("averageAge: ", averageAge);
+console.log("\n");
 // Create an array called names that contains the following values: 'Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.
 // Use a loop to iterate through the array and calculate the average number of letters per name.
 // Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
@@ -23,13 +30,16 @@ let allNames = '';
 for (let i = 0; i < names.length; i++) {
   allNames += names[i] + ' ';
 }
-
+console.log("\n");
 // How do you access the last element of any array?
 let arr = [1, 2, 3, 4, 5]; // example array
 arr[arr.length - 1]; // 5 
 
 // How do you access the first element of any array?
 arr[0]; // 1
+console.log("first element: ", arr[0]);
+console.log("last element: ", arr[arr.length - 1]);
+console.log("\n");
 // Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
 // For example:
  // starting with this array
@@ -38,6 +48,9 @@ const nameLengths = [];
 for (let i = 0; i < names.length; i++) {
   nameLengths.push(names[i].length);
 }
+console.log("nameLengths: ", nameLengths);
+console.log("\n");
+
 // Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array.
 let nameLengthsSum = 0;
 for(let i = 0; i < nameLengths.length; i++){
@@ -52,7 +65,7 @@ const concatenateWord = (word, n) => {
   }
   return result;
 }
-
+console.log("concatenateWord: ", concatenateWord('Hello', 3));
 // Write a function that takes two parameters, firstName and lastName, and returns a full name. The full name should be the first and the last name separated by a space.
 const getFullName = (firstName, lastName) => {
   return firstName + ' ' + lastName;
@@ -65,6 +78,7 @@ const isSumGreaterThan100 = (numbers) => {
   }
   return sum > 100;
 }
+console.log("isSumGreaterThan100: ", isSumGreaterThan100([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 
 // Write a function that takes an array of numbers and returns the average of all the elements in the array.
 const getAverage = (numbers) => {
@@ -74,6 +88,8 @@ const getAverage = (numbers) => {
   }
   return sum / numbers.length;
 }
+console.log("\n");
+console.log("getAverage: ", getAverage([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 // Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 const isAverageGreaterThan = (numbers1, numbers2) => {
   let sum1 = 0;
@@ -90,10 +106,14 @@ const isAverageGreaterThan = (numbers1, numbers2) => {
 
   return average1 > average2;
 }
+console.log("isAverageGreaterThan: ", isAverageGreaterThan([1, 2, 3, 4, 5], [6, 7, 8, 9, 10]));
+console.log("\n");
 // Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 const willBuyDrink = (isHotOutside, moneyInPocket) => {
   return isHotOutside && moneyInPocket > 10.50;
 }
+console.log("willBuyDrink: ", willBuyDrink(true, 11));
+console.log("\n");
 // Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
 // This function takes an array of numbers and returns the sum of all the numbers in the array.
 const sumArray = (numbers) => {
@@ -103,3 +123,5 @@ const sumArray = (numbers) => {
   }
   return sum;
 }
+console.log("sumArray: ", sumArray([1, 2, 3, 4, 5]));
+console.log("\n");
